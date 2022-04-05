1f261031e663deab0dd895c5a8fe0032ea0a7bbd
Dio Lemos <pedrodiogenes@outlook.com>
2022-04-05 18:15:46 -0300

    basic files creation
    created the files thatwill be used in the project

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path')
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/index.js')
+    },
+    output:{
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            title: 'EuroGrill'
+        })
+    ],
+    module: {
+        rules: [ {test: /\.js$/,
+        exclude: /mode_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }},{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        },
+        {
+            test: /\.(gif|png|jpg|jpeg|svg)$/i,
+            type: 'asset/resource'
+        }
+            
+
+        ]
+    }
+}
