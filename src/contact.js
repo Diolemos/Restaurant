16ec206a3c018ba51da100e64a7373c91e427ce4
Dio Lemos <pedrodiogenes@outlook.com>
2022-09-08 01:24:04 -0300

    format

--- src/contact.js
+++ src/contact.js
@@ -1,29 +1,29 @@
-import map from './assets/restaurant-location.png'
+import map from "./assets/restaurant-location.png";
 function createContact() {
-    const contact = document.createElement('div')
-    contact.classList.add('contact')
-  
-    const phoneNumber = document.createElement('p')
-    phoneNumber.textContent = '☎️ 123 456 789'
-  
-    const address = document.createElement('p')
-    address.textContent = '📍 Bacon road 55, San Antonio, TX'
-  
-    const restaurantLocation = document.createElement('img')
-    restaurantLocation.src = map
-    restaurantLocation.alt = 'Burger Duke restaurant location'
-  
-    contact.appendChild(phoneNumber)
-    contact.appendChild(address)
-    contact.appendChild(restaurantLocation)
-  
-    return contact
-  }
-  
-  function loadContact() {
-    const main = document.getElementById('main')
-    main.textContent = ''
-    main.appendChild(createContact())
-  }
-  
-  export default loadContact
+  const contact = document.createElement("div");
+  contact.classList.add("contact");
+
+  const phoneNumber = document.createElement("p");
+  phoneNumber.textContent = "☎️ 123 456 789";
+
+  const address = document.createElement("p");
+  address.textContent = "📍 Bacon road 55, San Antonio, TX";
+
+  const restaurantLocation = document.createElement("img");
+  restaurantLocation.src = map;
+  restaurantLocation.alt = "Burger Duke restaurant location";
+
+  contact.appendChild(phoneNumber);
+  contact.appendChild(address);
+  contact.appendChild(restaurantLocation);
+
+  return contact;
+}
+
+function loadContact() {
+  const main = document.getElementById("main");
+  main.textContent = "";
+  main.appendChild(createContact());
+}
+
+export default loadContact;
