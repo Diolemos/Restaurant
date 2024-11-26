6cca4c93af58b7a3a3f50e41401d786e80269bbb
Dio Lemos <pedro.devsc@gmail.com>
2024-11-26 17:09:41 -0300

    update packages, update webpack.config

--- src/menu.js
+++ src/menu.js
@@ -1,107 +1,109 @@
-import jrduke from './assets/burgers/jrduke.png';
-import burgerDuke from './assets/burgers/burgerduke.png'
-import bbqDuke from './assets/burgers/bbqDuke.png'
-import crispyFish from './assets/burgers/crispyfish.png'
-import doubleDuke from './assets/burgers/doubleduke.png';
-import ohDeer from './assets/burgers/ohdeer.png';
-import omelet from './assets/burgers/omeletduke.png';
-import kingBuster from './assets/burgers/kingbuster.png';
-import keto from './assets/burgers/ketoburger.png'
-import el from './assets/burgers/elpasospecial.png';
-
-
+import jrduke from "./assets/burgers/jrduke.png";
+import burgerDuke from "./assets/burgers/burgerduke.png";
+import bbqDuke from "./assets/burgers/bbqduke.png";
+import crispyFish from "./assets/burgers/crispyfish.png";
+import doubleDuke from "./assets/burgers/doubleduke.png";
+import ohDeer from "./assets/burgers/ohdeer.png";
+import omelet from "./assets/burgers/omeletduke.png";
+import kingBuster from "./assets/burgers/kingbuster.png";
+import keto from "./assets/burgers/ketoburger.png";
+import el from "./assets/burgers/elpasospecial.png";
 
 function createMenu() {
-    const menu = document.createElement("div");
-    menu.classList.add("menu");
-  
-    menu.appendChild(
-        
-        //Need to create menu items... for now, I will use these placeholders
-      createMenuItem(
-        "Double Duke", doubleDuke,
-        "bread, cheese, bacon, onions, garlic, double patty, bacon, tomato"
-      )
-    );
-    menu.appendChild(
-        createMenuItem(
-            "Burger Duke", burgerDuke,
-            "bread, cheese, cherry tomatoes, lettuce, garlic."
-        )
-      );
-      menu.appendChild(
-        createMenuItem(
-            "Barbecue Duke", bbqDuke,
-            "bread, cheese, bacon, onions, garlic, patty, barbecue sauce, bacon"
-        )
-      );
-      menu.appendChild(
-        createMenuItem(
-            "Crispy Fish ", crispyFish,
-            "bread, cheese, bacon, onions, garlic, bacon,fried fish patty"
-        )
-      );
-      menu.appendChild(
-        createMenuItem(
-            "Oh Deer", ohDeer,
-            "bread, cheese, bacon, onions, garlic, bacon, deer patty, lettuce, bbq sauce"
-        )
-      );
-      menu.appendChild(
-        createMenuItem(
-            "Omelette burger", omelet,
-            "bread, cheese, bacon, onions, garlic, omelette, patty, bacon"
-        )
-      );
-      menu.appendChild(
-        createMenuItem(
-            "King Buster", kingBuster,
-            "bread, cheese,double patty, bacon, onions,special sauce, garlic, bacon, bacon, bacon"
-        )
-      );
-      menu.appendChild(
-        createMenuItem(
-            "Keto burger", keto,
-            "patty, tomatoes, cheese"
-        )
-      );
-      menu.appendChild(
-        createMenuItem(
-            "El Paso Special", el,
-            "patty,bread, tomatoes, cheese,beans, hot sauce, tostilhas"
-        )
-      );
-  
-    return menu;
-  }
-  
-  //That function creates the menu items, it is called inside the createMenu function, its hoisted, of course 
-  function createMenuItem(name,link, description) {
-    const menuItem = document.createElement("div");
-    menuItem.classList.add("menu-item");
-  
-    const foodName = document.createElement("h2");
-    foodName.textContent = name;
-  
-    const foodDescription = document.createElement("p");
-    foodDescription.textContent = description;
-    
-    //change logic to work with tow words names
-    const foodImage = document.createElement("img");
-    foodImage.src = link;
-    foodImage.alt = name;
-  
-    menuItem.appendChild(foodImage);
-    menuItem.appendChild(foodName);
-    menuItem.appendChild(foodDescription);
-  
-    return menuItem;
-  }
-  
-  function loadMenu() {
-    const main = document.getElementById("main");
-    main.textContent = "";
-    main.appendChild(createMenu());
-  }
-  
-  export default loadMenu;
+  const menu = document.createElement("div");
+  menu.classList.add("menu");
+
+  menu.appendChild(
+    //Need to create menu items... for now, I will use these placeholders
+    createMenuItem(
+      "Double Duke",
+      doubleDuke,
+      "bread, cheese, bacon, onions, garlic, double patty, bacon, tomato"
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      "Burger Duke",
+      burgerDuke,
+      "bread, cheese, cherry tomatoes, lettuce, garlic."
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      "Barbecue Duke",
+      bbqDuke,
+      "bread, cheese, bacon, onions, garlic, patty, barbecue sauce, bacon"
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      "Crispy Fish ",
+      crispyFish,
+      "bread, cheese, bacon, onions, garlic, bacon,fried fish patty"
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      "Oh Deer",
+      ohDeer,
+      "bread, cheese, bacon, onions, garlic, bacon, deer patty, lettuce, bbq sauce"
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      "Omelette burger",
+      omelet,
+      "bread, cheese, bacon, onions, garlic, omelette, patty, bacon"
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      "King Buster",
+      kingBuster,
+      "bread, cheese,double patty, bacon, onions,special sauce, garlic, bacon, bacon, bacon"
+    )
+  );
+  menu.appendChild(
+    createMenuItem("Keto burger", keto, "patty, tomatoes, cheese")
+  );
+  menu.appendChild(
+    createMenuItem(
+      "El Paso Special",
+      el,
+      "patty,bread, tomatoes, cheese,beans, hot sauce, tostilhas"
+    )
+  );
+
+  return menu;
+}
+
+//That function creates the menu items, it is called inside the createMenu function, its hoisted, of course
+function createMenuItem(name, link, description) {
+  const menuItem = document.createElement("div");
+  menuItem.classList.add("menu-item");
+
+  const foodName = document.createElement("h2");
+  foodName.textContent = name;
+
+  const foodDescription = document.createElement("p");
+  foodDescription.textContent = description;
+
+  //change logic to work with tow words names
+  const foodImage = document.createElement("img");
+  foodImage.src = link;
+  foodImage.alt = name;
+
+  menuItem.appendChild(foodImage);
+  menuItem.appendChild(foodName);
+  menuItem.appendChild(foodDescription);
+
+  return menuItem;
+}
+
+function loadMenu() {
+  const main = document.getElementById("main");
+  main.textContent = "";
+  main.appendChild(createMenu());
+}
+
+export default loadMenu;
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,51 @@
-const path = require('path')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output:{
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-   
-    
-    module: {
-        rules: [ {test: /\.js$/,
-        exclude: /mode_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env']
-            }
-        }},{
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-        },
-        {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-          },
-            
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
 
-        ]
-    }
-}
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // Path to your source HTML file
+      filename: "index.html", // Name of the output HTML file
+    }),
+  ],
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+  },
+};
